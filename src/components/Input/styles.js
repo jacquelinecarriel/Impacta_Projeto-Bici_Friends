a27b6860748244edfb6b6b6d3fde9cb1c505122e
Jacqueline Carriel <jacqueline.carriell@gmail.com>
2025-03-09 21:55:07 -0300

    Add init

---
+++ src/components/Input/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Input = styled.input`
+    outline: none;
+    padding: 16px 20px;
+    width: 100%;
+    font-size: 16px;
+    
+    background-color: #f0f2f5;
+    border: none;
+    
+`;
